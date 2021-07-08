5af017bd645dfda9cecb5eecc5dac64ac34a2cb8
Tamcaolehao <67093882+Tamcaolehao@users.noreply.github.com>
2021-07-08 22:41:05 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-	$connect= mysqli_connect("localhost","root","");
-	mysqli_select_db($connect, "quanao");
+	$connect= mysqli_connect("us-cdbr-east-04.cleardb.com","b97c99c06a6131","d35f7ee4");
+	mysqli_select_db($connect, "heroku_6ac4263a8aebd63");
 	mysqli_query($connect, "SET names 'utf8'");
 	if(!$connect){
 		echo "Không thể kết nối đến Database!".mysqli_connect_error($connect);
